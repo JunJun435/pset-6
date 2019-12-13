eb44825f05d9888737efdec170d231f760751153
Jun Yang <JYang@ucvts.tec.nj.us>
2019-12-13 10:02:52 -0500

    Omae Wa Mou Shinderu

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    document.getElementById("adding").onclick = addAnItem;
+    document.getElementById("priority").onclick = settingPriority;
+    document.getElementById("marking").onclick = markingAnItem;
+    document.getElementById("removing").onclick = removingAnItem;
+}
+
+//Addinng an item to the list
+
+const addingAnItem = function() {
+
+}
