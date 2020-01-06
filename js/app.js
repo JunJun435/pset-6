6343508b44abb5f692a6de7d8f72db4a084fa9a1
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-01-05 19:13:32 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
+let elements = []
+var add = document.getElementById("add_button");
+var row = document.getElementsByClassName("row");
+let list_item = document.getElementsByClassName("to_do")
+let priority_button = document.getElementsByClassName("priority_button");
+let complete_button = document.getElementsByClassName("complete_button");
+let remove_button = document.getElementsByClassName("remove_button");
+
+
+let element_prioritize;
+
 window.onload = function() {
-    document.getElementById("adding").onclick = addAnItem;
-    document.getElementById("priority").onclick = settingPriority;
-    document.getElementById("marking").onclick = markingAnItem;
-    document.getElementById("removing").onclick = removingAnItem;
+  document.getElementById("table").onmouseover = startup;
 }
 
-//Addinng an item to the list
-
-const addingAnItem = function() {
-let addItems = prompt("What do you want to add?");
-if (addItems == null) {
-
-} else {
-    let
-}
-}
